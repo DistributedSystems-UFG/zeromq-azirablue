d968cc7275ebd9019e01ebccbb5dce17b47d1c46
Ana Luísa de Bastos Chagas <161107603+azirablue@users.noreply.github.com>
2026-05-31 14:34:59 -0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+MACHINE_A = "44.202.179.145"   # producer / server / publisher     # server
+MACHINE_B = "98.84.55.114"   # middle / client / subscriber    # peer1
+MACHINE_C = "34.205.26.151"   # consumer final                 # peer2
+
+PORT_1 = "5678"
+PORT_2 = "5679"
+PORT_3 = "5680"
